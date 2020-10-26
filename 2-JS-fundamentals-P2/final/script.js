60cfef92ceba16e47fcd2f262a7d9cd0ec13750f
Yaroslav <boyaal24@gmail.com>
2020-10-26 20:59:30 +0200

    finished section 2

--- 2-JS-fundamentals-P2/final/script.js
+++ 2-JS-fundamentals-P2/final/script.js
@@ -192,6 +192,108 @@ if (mark.bmi > john.bmi) {
     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is lower than ${john.fullName}'s BMI (${john.bmi})`);
 } */
 
-for (let i = 0; i < 10; i++){
+/* for (let i = 0; i < 10; i++){
     console.log(`Lifting weight repetion ${i} 🎾`);
+} */
+
+/* const years = [1991, 2007, 1969, 2020];
+
+const ages = [];
+
+for (let i = 0; i < years.length; i++){
+    ages.push(2037 - years[i])
 }
+console.log(ages);
+ */
+
+/* const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = []; */
+
+//continue and break
+/* console.log('-----ONLY STRINGS------');
+
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+    console.log(jonas[i], typeof jonas[i]);
+} */
+
+/* console.log('-----BREAK WITH NUMBER------');
+
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i]);
+} */
+
+/* const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = jonas.length - 1; i >= 0; i--){
+    console.log(i, jonas[i]);
+}
+ */
+/* for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-------Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++){
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🎭`);
+    }
+} */
+
+/* let rep = 1;
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep} 📽`);
+    rep++;
+}
+ */
+/* let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
+} */
+
+
+const calcTips = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+
+const totals = [];
+
+for (let i = 0; i < bills.length; i++){
+    const tip = calcTips(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage([2, 3, 6]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
