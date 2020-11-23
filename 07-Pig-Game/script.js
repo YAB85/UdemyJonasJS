2ee78f3228354a0002e26ffe4c5192dd07275bdd
Yaroslav <boyaal24@gmail.com>
2020-11-23 22:49:11 +0200

    finished prj pig-game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -14,15 +14,27 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
 
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,6 +43,7 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+init();
 
 // Rolling dice functionality
 
@@ -67,7 +80,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
     //2. Check if player`s score is >= 100
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -83,3 +96,5 @@ btnHold.addEventListener('click', function () {
     // Switch to the next player
   }
 });
+
+btnNew.addEventListener('click', init);
