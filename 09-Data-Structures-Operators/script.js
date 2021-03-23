7133dc0e936116c520e3d3bb6b012e3736c92b23
yaroslavBova <yabova@ukr.net>
2021-03-23 22:47:28 +0200

    section 9

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,6 +1,22 @@
 'use strict';
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-/* const restaurant = {
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day - ${2 + 4}`]: {
+    open: 0,
+    close: 24,
+  },
+};
+
+const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
@@ -49,7 +65,7 @@ restaurant.orderDelivery({
   mainIndex: 2,
   starterIndex: 2,
 });
- */
+
 /* const { name, openingHours, categories } = restaurant;
 
 console.log(name, openingHours, categories);
@@ -246,6 +262,44 @@ console.log(false && null && '' && undefined); */
 
 console.log(guestsCorrect); */
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) {
+  console.log(item);
+}
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+console.log(menu.entries());
+
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//Methods
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+console.log(users[0]?.name ?? 'Users array empty');
+
+/* ********** Coding Challenge #1 *************** */
 /* 
 We're building a football betting app (soccer for my American friends 😅)!
 
@@ -264,7 +318,7 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
-const game = {
+/* const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
   players: [
@@ -347,3 +401,4 @@ printGoal('Davies', 'Muller');
 
 team1 < team2 && console.log(`Team  1 is more likely to win`);
 team1 > team2 && console.log(`Team  2 is more likely to win`);
+ */
